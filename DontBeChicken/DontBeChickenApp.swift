eb9722ee617d542020f802d13a3299c57a525a7c
comatoki <hbabnd@gmail.com>
2024-04-20 22:12:28 +0900

    Initial Commit

---
+++ DontBeChicken/DontBeChickenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontBeChickenApp.swift
+//  DontBeChicken
+//
+//  Created by COMATOKI on 2024-04-20.
+//
+
+import SwiftUI
+
+@main
+struct DontBeChickenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
